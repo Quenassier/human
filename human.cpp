b5a170992c81801f470c3dba48099c013b07aa29
Quenassier <112952435+Quenassier@users.noreply.github.com>
2023-03-30 01:02:31 +0700

    Update human.cpp

--- human.cpp
+++ human.cpp
@@ -7,49 +7,49 @@ using namespace std;
 
 class persen {
 public:
-  int age;
-  std::string national, name;
-  bool pol;
-  
-  persen(int age, std::string national, std::string name, bool pol) {
-    this->age = age;
-    this->national = national;
-    this->name = name;
-    this->pol = pol;
+    int age;
+    std::string national, name;
+    bool pol;
+
+    persen(int age, std::string national, std::string name, bool pol) {
+        this->age = age;
+        this->national = national;
+        this->name = name;
+        this->pol = pol;
 
 
-  }
-  void addage(int age) {
-    this->age += age;
+    }
+    void addage(int age) {
+        this->age += age;
 
-   }
-  void smenapola() {
-    this->pol =! pol;
-    if (pol == 1)
-    cout<< "man";
-    if (pol == 0)
-      cout << "woman";
-  }
-  void say(persen* people) {
-    cout << this->name << " Hi! "<<people->name<<"\t";
-  }
-  ~persen() {
-  
-    cout << "i delet";
-  }
+    }
+    void smenapola() {
+        this->pol = !pol;
+        if (pol == 1)
+            cout << "man";
+        if (pol == 0)
+            cout << "woman";
+    }
+    void say(persen* people) {
+        cout << this->name << " Hi! " << people->name << "\t";
+    }
+    ~persen() {
+
+        cout << "i delet";
+    }
 
 };
 
-int main(){
-  persen* herbert = new persen(20, "USA","herbert",1);
-  persen* anna = new persen(30, "russ", "anna", 0);
-  cout << herbert->name<<"\t";
-  cout << anna->vozrast << "\t";
-  anna->addvozrast(20);
-  cout << anna->vozrast<<"\t";
-  herbert->smenapola();
-  cout << herbert->pol<<"\t";
-  herbert->say(herbert);
-  delete(anna);
-  cout << anna->name << "\t";
+int main() {
+    persen* herbert = new persen(20, "USA", "herbert", 1);
+    persen* anna = new persen(30, "russ", "anna", 0);
+    cout << herbert->name << "\t";
+    cout << anna->vozrast << "\t";
+    anna->addvozrast(30);
+    cout << anna->vozrast << "\t";
+    herbert->smenapola();
+    cout << herbert->pol << "\t";
+    herbert->say(herbert);
+    delete(anna);
+    cout << anna->name << "\t";
 }
